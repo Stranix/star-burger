c825a219462e88d10500283ebf5191ecd630549d
Stranix <Phantom2525@gmail.com>
2023-07-10 18:13:41 +0300

    add ui showing the distance to the restaurant

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -1,4 +1,7 @@
+import requests
+
 from django import forms
+from django.conf import settings
 from django.shortcuts import redirect, render
 from django.views import View
 from django.urls import reverse_lazy
@@ -7,6 +10,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 
+from geopy import distance
 
 from foodcartapp.models import Product, Restaurant
 from foodcartapp.models import Order
@@ -71,8 +75,10 @@ def view_products(request):
 
     products_with_restaurant_availability = []
     for product in products:
-        availability = {item.restaurant_id: item.availability for item in product.menu_items.all()}
-        ordered_availability = [availability.get(restaurant.id, False) for restaurant in restaurants]
+        availability = {item.restaurant_id: item.availability for item in
+                        product.menu_items.all()}
+        ordered_availability = [availability.get(restaurant.id, False) for
+                                restaurant in restaurants]
 
         products_with_restaurant_availability.append(
             (product, ordered_availability)
@@ -91,8 +97,60 @@ def view_restaurants(request):
     })
 
 
+def fetch_coordinates(address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": settings.YANDEX_API_KEY,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection'][
+        'featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lat, lon
+
+
+def get_distance(address_from, address_to):
+    coords_from = fetch_coordinates(address_from)
+    coords_to = fetch_coordinates(address_to)
+    return distance.distance(coords_from, coords_to).km
+
+
+def distance_formatter(distance):
+    distance_with_units = f"{int(distance)} км"
+    if distance < 10:
+        distance_with_units = f"{distance:.2f} км"
+    if distance < 1:
+        distance_with_units = f"{int(distance * 1000)} м"
+    return distance_with_units
+
+
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
+    orders = Order.objects.fetch_with_price().suitable_restaurants()
+    for order in orders:
+        try:
+            for restaurant in order.suitable_restaurants:
+                restaurant.distance = get_distance(
+                    order.address,
+                    restaurant.address,
+                )
+                restaurant.readable_distance = distance_formatter(
+                    restaurant.distance
+                )
+            suitable_restaurants = sorted(
+                order.suitable_restaurants,
+                key=lambda restaurant: restaurant.distance
+            )
+            order.suitable_restaurants = suitable_restaurants
+        except AttributeError:
+            pass
     return render(request, template_name='order_items.html', context={
-        'order_items': Order.objects.fetch_with_price().suitable_restaurants()
+        'order_items': orders
     })
