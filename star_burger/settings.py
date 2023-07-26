4e62d7b4dcf59012a24a7d39efd8ad6d286c01dc
Stranix <Phantom2525@gmail.com>
2023-07-26 16:34:20 +0300

    установка дефолтного значения для ROLLBAR_ACCESS_TOKEN по умолчанию не задано

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -133,7 +133,7 @@ INTERNAL_IPS = [
 ]
 
 ROLLBAR = {
-    'access_token': env('ROLLBAR_ACCESS_TOKEN'),
+    'access_token': env.str('ROLLBAR_ACCESS_TOKEN', ''),
     'environment': env.str('ROLLBAR_ENV', 'development'),
     'code_version': '1.0',
     'root': BASE_DIR,
